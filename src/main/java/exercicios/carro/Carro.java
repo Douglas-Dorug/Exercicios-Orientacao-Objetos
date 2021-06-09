b78486774c755440ec4bc67dc1100d06d7727cbf
Douglas-Dorug <douglasx30@gmail.com>
2021-06-09 16:34:21 -0300

    Exercicio da aula 1 resolvido.

---
+++ src/main/java/exercicios/carro/Carro.java
@@ -0,0 +1,17 @@
+package exercicios.carro;
+
+public class Carro {
+    private String carro = "Carro";
+
+    private Integer lugaresNoCarro = 5;
+
+    private  Integer pessoasNoCarro = 5;
+
+    public Integer getLugaresNoCarro() {
+        return lugaresNoCarro;
+    }
+
+    public  Integer getPessoasNoCarro(){
+        return pessoasNoCarro;
+    }
+}
---
+++ src/main/java/exercicios/carro/EntrarCarro.java
@@ -0,0 +1,38 @@
+package exercicios.carro;
+
+import java.util.Scanner;
+
+public class EntrarCarro {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Carro carro = new Carro();
+
+
+        Integer entrarNoCarro = 0;
+        Integer lugaresDisponiveis = carro.getLugaresNoCarro() - carro.getPessoasNoCarro();
+
+        System.out.println("Este carro possui: " + carro.getLugaresNoCarro() + " lugares no total.");
+        System.out.println("Este carro possui: " + carro.getPessoasNoCarro() + " pessoas dentro no momento.");
+        System.out.println("Este carro possui: " + lugaresDisponiveis + " lugares disponiveis.");
+
+        if (lugaresDisponiveis >= 1){
+            System.out.println("Digite quantas pessoas vão entrar no carro");
+            entrarNoCarro = leitor.nextInt();
+            if (entrarNoCarro <= lugaresDisponiveis && entrarNoCarro >= 1){
+                System.out.println("Este carro agora possui: " + (carro.getPessoasNoCarro() + entrarNoCarro) + " pessoas dentro no momento.");
+            }else{
+                System.out.println("O valor digitado é invalido.");
+            }
+
+        }else{
+            System.out.println("Não há vagas disponiveis neste carro!");
+        }
+
+
+
+
+
+
+    }
+}
